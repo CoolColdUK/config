2d7c62082568c52fa46e4c7a7eb87bce7fe0b58d
CoolColdUK <coolcold@gmail.com>
2024-03-05 20:41:09 +0000

    add eslint config

---
+++ packages/eslint-config/index.js
@@ -0,0 +1,46 @@
+/**
+ * Config for packages
+ * - based on airbnb typescript
+ * - use with prettier
+ * @see https://stackoverflow.com/q/61963749/7058536
+ */
+module.exports = {
+  //required!; use the previously installed parser; it allows ESLint to understand
+  //TypeScript syntax; it converts TypeScript into an ESTree-compatible form so it
+  //can be used in ESLint
+  parser: '@typescript-eslint/parser',
+
+  plugins: ['@typescript-eslint'],
+  env: {
+    node: true,
+    jest: true,
+    es2021: true,
+  },
+  extends: ['airbnb-base', 'airbnb-typescript/base', 'prettier'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json', //required for "type-aware linting"
+  },
+  ignorePatterns: ['**/__generated__/*.ts'],
+  rules: {
+    'import/prefer-default-export': 'off',
+    'no-console': 'warn',
+    'no-await-in-loop': 'warn',
+    'class-methods-use-this': 'warn',
+    '@typescript-eslint/no-unused-vars': ['warn', {argsIgnorePattern: '^_'}],
+    '@typescript-eslint/dot-notation': ['warn', {allowIndexSignaturePropertyAccess: true}],
+    'import/no-cycle': [2, {ignoreExternal: true}], // can disable due to high computational cost
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+      },
+    },
+  },
+  ignorePatterns: ['.eslintrc.js', 'dist', '/*.*'],
+};
