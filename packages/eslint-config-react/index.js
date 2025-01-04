8b2d324cd3e3dfcbb340e04e1a24fe6ec551a851
CoolColdUK <coolcold@gmail.com>
2025-01-04 20:42:28 +0000

    skip dependency check for test files

--- packages/eslint-config-react/index.js
+++ packages/eslint-config-react/index.js
@@ -42,6 +42,8 @@ module.exports = {
       "error",
       {
         "devDependencies": [
+          "**/*.test.*",
+          "**/*.spec.*",
           "**/*.stories.*",
           "**/.storybook/**/*.*"
         ],
