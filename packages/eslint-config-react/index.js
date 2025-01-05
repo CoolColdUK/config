e2966773bcbfab6863350feba73c24c2061580f1
CoolColdUK <coolcold@gmail.com>
2025-01-05 00:36:32 +0000

    fix eslint error

--- packages/eslint-config-react/index.js
+++ packages/eslint-config-react/index.js
@@ -41,7 +41,7 @@ module.exports = {
     "import/no-extraneous-dependencies": [
       "error",
       {
-        "dependencies": [
+        "devDependencies": [
           "**/*.test.*",
           "**/*.spec.*",
           "**/*.stories.*",
